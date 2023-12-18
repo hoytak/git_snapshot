5d6c1bc6868eef02b11ebec4f69cbbb2c1d77bac
Hoyt Koepke <hoytak@xethub.com>
2023-12-18 11:46:06 -0800

    Fixed argument parsing.

--- common.sh
+++ common.sh
@@ -7,28 +7,43 @@ show_help() {
     echo "Usage: $0 [OPTIONS]"
     echo
     echo "Options:"
-    echo "  --repo             Set the git repository to use for backup." 
-    echo "                     If omitted, the value from config.sh is used."
-    echo "  --local-data-store Set the local data store path to a given directory."
-    echo "                     If omitted, the value from config.sh is used."
-    echo "  --snapshot-dir     Set the directory to snapshot. Default is '$snapshot_dir'"
-    echo "  --backup-subdir    Set the directory within the git repo for snapshots. Default is '$backup_subdir'"
-    echo "  -h, --help         Show this help message and exit"
+    echo "  --repo                  Set the git repository to use for backup." 
+    echo "                          If omitted, the value from config.sh is used."
+    echo "  --local-data-store      Set the local data store path to a given directory."
+    echo "                          If omitted, the value from config.sh is used."
+    echo "  --snapshot-dir          Set the directory to snapshot. Default is '$snapshot_dir'"
+    echo "  --backup-subdir         Set the directory within the git repo for snapshots. Default is '$backup_subdir'"
+    echo "  --include-private-files Include files without group read permissions"
+    echo "  -h, --help              Show this help message and exit"
     echo
 }
 
 # Parsing arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
-        --repo) git_repo="$2"; shift ;;                 
-        --local-data-store) local_data_store="$2"; shift ;;   
+        --repo) git_repo="$2"; shift ;;
+        --repo=*) git_repo="${1#*=}" ;;
+        
+        --local-data-store) local_data_store="$2"; shift ;;
+        --local-data-store=*) local_data_store="${1#*=}" ;;
+        
         --snapshot-dir) snapshot_dir="$2"; shift ;;
+        --snapshot-dir=*) snapshot_dir="${1#*=}" ;;
+        
         --backup-subdir) backup_subdir="$2"; shift ;;
-        -h|--help) show_help; exit 0 ;;                 
+        --backup-subdir=*) backup_subdir="${1#*=}" ;;
+        
+        --repo-branch) repo_branch="$2"; shift ;;
+        --repo-branch=*) repo_branch="${1#*=}" ;;
+        
+        --include-private-files) include_private_files=true ;;
+        
+        -h|--help) show_help; exit 0 ;;
+        
         *) echo "Unknown parameter: $1"; show_help; exit 1 ;;
     esac
-    shift
 done
+
 
 # Set up the environment variables if needed. 
 if [[ ! -z $local_data_store ]] ; then 
@@ -45,6 +60,12 @@ if [[ ! -z $local_data_store ]] ; then
 
    export XET_CAS_SERVER="local://$local_data_store"
 fi
+
+if [[ -z "$git_repo" ]] ; then 
+    >&2 echo "Remote git repo not set.  Please set git_repo in config.sh to your remote repo, or pass in --repo on the command line."
+    exit 1
+fi
+
 
 # Make sure we have the normalized absolute path and it's accessable.
 snapshot_dir="$(cd "$snapshot_dir" && pwd)"
